1f47ef64925a616b32adf53ee5e682a968dfbf60
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-10-21 13:45:08 +0300

    first commit

---
+++ generic-variables.tf
@@ -0,0 +1,34 @@
+# Input Variables
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  type        = string
+  default     = ""
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
+  description = "AWS Secret Key"
+  sensitive   = true
+}
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+# Business Division
+variable "business_division" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type        = string
+  default     = "SpaceX"
+}
